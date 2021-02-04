a8171b119f3eb3248daef59b1706cb7451dc5ebb
vipulshah31120 <75607031+vipulshah31120@users.noreply.github.com>
2021-02-05 00:08:55 +0530

    List
    List comprehensions, queue, Dequeue,

---
+++ DailyClasses/04-02-2021/ds_list.py
@@ -0,0 +1,40 @@
+# list : Lists are used to store multiple items in a single variable.
+# Lists are one of 4 built-in data types in Python
+# the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+
+family = ["Pita Ji","Mata Ji","Bhrata Ji","Behan Ji","Dada Ji","Dadi Ji","Me","and 4 others"]
+print(family)
+
+# there many function for list
+# append(), extend()
+
+# Using Lists as Stacks
+family.append("pahli wali")
+family.append("dosri wali")
+print(family)
+
+family.pop()
+print("after pop: ")
+print(family)
+
+family.pop()
+print("after pop: ")
+print(family)
+
+# Using Lists as Queues
+# While appends and pops from the end of list are fast, doing inserts or pops from the beginning of a list is slow (because all of the other elements have to be shifted by one).
+# To implement a queue, use collections.deque which was designed to have fast appends and pops from both ends.
+
+from collections import deque
+gfQue = deque(["Muskan","Simran"])
+gfQue.append("Aishwarya")
+
+print(gfQue)
+
+gfQue.pop();
+print("pop");
+print(gfQue)
+
+gfQue.popleft();
+print("popleft");
+print(gfQue)
---
+++ DailyClasses/04-02-2021/list_comprehensions.py
@@ -0,0 +1,6 @@
+# List Comprehensions
+square = []
+for x in range(1,16):
+    square.append(x**2)
+
+print(square)
