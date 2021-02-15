567e3a759c5191ab4df68ebbbc5127c7ac736f2b
vipulshah31120 <75607031+vipulshah31120@users.noreply.github.com>
2021-02-15 22:02:54 +0530

    functions

--- DailyClasses/10-02-2021/fibonacci.py
+++ DailyClasses/10-02-2021/fibonacci.py
@@ -4,4 +4,4 @@ b = 1
 while a<11 :
     print(a,end='')
 
-a+b,a=a+b
+a,a+b,a=a+b
--- DailyClasses/12-02-2021/practice.py
+++ DailyClasses/12-02-2021/practice.py
@@ -1,2 +1,61 @@
 print("Kamaaal hai ji..., try CTRL+B")
 print("HIHIHIHI")
+
+x = lambda a : a + 20 
+print(x(22))
+
+x = lambda a,b : a*b
+print(x(34,56))
+
+x = lambda a,b,c : a+b*c
+print(x(2,4,5))
+
+def myfunc(n) :
+	return lambda a : a*n
+
+mydoubler = myfunc(4)	# n=4
+print(mydoubler(5)) 	# a=5
+
+mytripler = myfunc(6)	#n=6
+print(mytripler(8))		#n=8
+
+#Calculating Simple Interest
+def SI(r) :
+	return lambda p,t : p*r*t/100
+
+CalculateSI=SI(10)
+SIforme=CalculateSI(100,4)
+print(SIforme)
+
+for n in range(1,101) :
+	if n%3==0 and n%5==0 :
+		print("FizzBuzz")
+
+	elif n%3==0 :
+		print("Fizz")
+
+	elif n%5==0 :
+		print("Buzz")
+
+	else :
+		print(n)
+
+
+def FizzBuzz(n) :
+	if n%3==0 and n%5==0 :
+		print("FizzBuzz")
+
+	elif n%3==0 :
+		print("Fizz")
+
+	elif n%5==0 :
+		print("Buzz")
+
+	else :
+		print(n)
+
+FizzBuzz(100)
+
+
+
+
